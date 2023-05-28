113904cab1bd84850bb49d3c68943379be9e20ea
53911453+brianpavon@users.noreply.github.com <53911453+brianpavon@users.noreply.github.com>
2023-05-27 22:51:43 -0300

    ok sala chat a

--- src/app/components/sala-chat-a/sala-chat-a.page.ts
+++ src/app/components/sala-chat-a/sala-chat-a.page.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Mensajes } from 'src/app/interfaces/mensajes';
 import { AuthService } from 'src/app/services/auth.service';
 import { MensajesService } from 'src/app/services/mensajes.service';
+import { IonContent } from '@ionic/angular';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-sala-chat-a',
@@ -14,6 +16,8 @@ export class SalaChatAPage implements OnInit {
   isLogged:boolean = false;
   chatOn:boolean = false;
   usuarioLogueado : any;
+  cargando : boolean = true;
+  @ViewChild(IonContent) content!: IonContent;
 
   constructor(private auth : AuthService,private servMsj : MensajesService) { }
 
@@ -27,22 +31,37 @@ export class SalaChatAPage implements OnInit {
     this.cargarMensajes();
   }
 
-  ngAfterViewChecked():void{
-    //this.scrollAlUltimo();
+  ngAfterViewChecked():void{    
+    this.scrollToBottom();
+  }
+
+  scrollToBottom(): void {
+    this.content.scrollToBottom(0);
   }
 
   enviarMensaje(){
     if(this.mensajeNuevo == '') return;
-    //console.log(this.mensajeNuevo);
+    if(this.mensajeNuevo.length > 21){
+      Swal.fire({
+        title: 'Mensaje muy largo.',
+        icon: 'error',
+        text: `Se aceptan 21 caracteres máximo`,
+        confirmButtonText: "Aceptar",
+        confirmButtonColor: '#7e34bc',
+        background: '#000000',
+        color: '#FFFFFF',
+        heightAuto: false
+      });      
+      return
+    }
+    
+  
     let date = new Date();
-    let dateString = date.toString();
-    //sacamos el nombre del mail
-    let indice = this.usuarioLogueado.email.indexOf("@");    
-    let nombre = this.usuarioLogueado.email.substring(0, indice);
+    let dateString = date.toString();  
 
     let nuevoMsj ={
       id:this.arrayMensajes.length+1,
-      nombre: nombre,
+      nombre: this.usuarioLogueado.email,
       emisor:this.usuarioLogueado.uid,
       texto: this.mensajeNuevo,
       fecha: dateString,
@@ -50,29 +69,20 @@ export class SalaChatAPage implements OnInit {
     }    
     this.servMsj.guardarMensaje(nuevoMsj);
     this.mensajeNuevo = "";
-    // setTimeout(() => {
-    //   this.scrollAlUltimo();
-    // }, 20);
+    setTimeout(() => {
+      this.scrollToBottom();
+    }, 20);
   }
-
-  // scrollAlUltimo(){
-  //   let todosLosMsj:any = document.getElementsByClassName('msjChat');    
-  //   let ultimoMsj :any = todosLosMsj[(todosLosMsj.length - 1)];
-  //   let posicionSuperior;
-  //   if(ultimoMsj){
-  //     posicionSuperior = ultimoMsj.offsetTop;
-  //   }
-
-  //   //@ts-ignore
-  //   document.getElementById('contenedorMsj')?.scrollTop = posicionSuperior;
-  // }
 
   cargarMensajes(){
     this.servMsj.traerTodosLosMensajes('A').subscribe(
       mensajes=>{
         //console.log(mensajes);
         this.arrayMensajes = mensajes;
-        this.ordenarMensajes();        
+        this.ordenarMensajes();
+        setTimeout(() => {
+          this.cargando = false;
+        }, 1500);
       }
     )    
   }
